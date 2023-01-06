2bafe4a4cd2595ca190d761ab0d72b6d2c2794a6
radenpioneer <radenpioneer@users.noreply.github.com>
2023-01-06 19:57:43 +0700

    revamp font sizes

--- src/components/home/hero/hero.tsx
+++ src/components/home/hero/hero.tsx
@@ -12,8 +12,12 @@ export const Hero: FC = () => (
   <section className={style.__section}>
     <div className={style.__grid}>
       <article className={style.__headline}>
-        <h1 className={style.__heading}>{HEADLINE.title}</h1>
-        <p className="text-lg md:text-2xl 2xl:text-3xl">{HEADLINE.text}</p>
+        <h1
+          className={`${style.__heading} 2xl:text-8xl md:text-7xl sm:text-6xl text-4xl`}
+        >
+          {HEADLINE.title}
+        </h1>
+        <p className="text-lg md:text-2xl">{HEADLINE.text}</p>
       </article>
       <div className={style.__illustration}>
         <DigitalTransformIllustration />
