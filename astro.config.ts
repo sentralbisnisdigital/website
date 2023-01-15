0f46acca7a4087af227e13cb2dc7d20ffa6e4b66
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-01-15 14:41:12 +0000

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -2,16 +2,13 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import Icons from 'unplugin-icons/vite'
 import svgr from 'vite-plugin-svgr'
-
-// https://astro.build/config
 import compress from 'astro-compress'
-
-// https://astro.build/config
 import critters from 'astro-critters'
+import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), compress(), critters()],
+  site: 'https://www.sentralbisnisdigital.co.id',
+  integrations: [react(), compress(), critters(), sitemap()],
   vite: {
     plugins: [
       svgr(),
