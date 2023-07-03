26b3f377bbf070cac667515a0b5fc350f1fd9778
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-07-03 04:42:58 +0000

    try implementing hero component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,32 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, sharpImageService } from 'astro/config'
 
 // Astro integrations
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
-import AstroPWA from '@vite-pwa/astro'
-import mdx from '@astrojs/mdx'
+// import AstroPWA from '@vite-pwa/astro'
+import markdoc from '@astrojs/markdoc'
 
 // Vite plugins
 import Icons from 'unplugin-icons/vite'
 import svgr from 'vite-plugin-svgr'
 
-import manifest from './src/includes/manifest.json' assert { type: 'json' }
+// import manifest from './src/includes/manifest.json' assert { type: 'json' }
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sbdigital.id',
   integrations: [
     react(),
-    mdx(),
+    markdoc(),
     sitemap(),
-    AstroPWA({
-      strategies: 'injectManifest',
-      srcDir: 'src/includes',
-      filename: 'sw.ts',
-      registerType: 'autoUpdate',
-      manifest,
-    }),
+    // AstroPWA({
+    //   disable: true,
+    //   strategies: 'injectManifest',
+    //   srcDir: 'src/includes',
+    //   filename: 'sw.ts',
+    //   registerType: 'autoUpdate',
+    //   manifest,
+    // }),
   ],
   vite: {
     plugins: [
@@ -35,5 +36,11 @@ export default defineConfig({
         jsx: 'react',
       }),
     ],
+  },
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: sharpImageService(),
   },
 })
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,23 @@
+import type { FC, PropsWithChildren } from 'react'
+import type { GetImageResult } from 'astro'
+
+const HeroComp: FC<
+  PropsWithChildren<{ title: string; image: GetImageResult }>
+> = ({ title, image, children }) => (
+  <div>
+    <div>
+      <img
+        src={image.src}
+        width={image.options.width}
+        height={image.options.height}
+        alt={title}
+      />
+    </div>
+    <div>
+      <h1>{title}</h1>
+      <>{children}</>
+    </div>
+  </div>
+)
+
+export default HeroComp
