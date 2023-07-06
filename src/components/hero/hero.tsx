22f5221770de467e4e00e5a272a487904c529c20
R. Ilham <radenpioneer@users.noreply.github.com>
2023-07-06 15:53:32 +0700

    revamp hero section

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -5,25 +5,23 @@ import style from './hero.module.scss'
 const HeroComp: FC<
   PropsWithChildren<{ title: string; image: GetImageResult }>
 > = ({ title, image, children }) => (
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center max-w-screen-lg xl:max-w-screen-2xl min-h-full mx-auto">
-    <div>
-      <img
-        src={image.src}
-        width={image.options.width}
-        height={image.options.height}
-        alt={title}
-      />
-      {/** <a href="https://storyset.com/online">Online illustrations by Storyset</a> */}
+  <article className="flex flex-col gap-4 lg:gap-6 xl:gap-8 max-w-screen-md 2xl:max-w-screen-lg mx-auto py-8 md:py-16 2xl:py-24">
+    <h1
+      className={`${style.__title} text-4xl lg:text-6xl 2xl:text-8xl !leading-tight font-black`}
+    >
+      {title}
+    </h1>
+    <>{children}</>
+    <div className="flex flex-row flex-wrap items-center gap-4">
+      <a className="btn md:btn-lg btn-link no-underline !p-0" href="#">
+        Lihat portofolio kami
+      </a>
+      <span className="md:text-lg">atau</span>
+      <a className="btn btn-primary md:btn-lg" href="#">
+        Kontak kami
+      </a>
     </div>
-    <div className="flex flex-col gap-4 lg:gap-6 xl:gap-8">
-      <h1
-        className={`${style.__title} text-2xl md:text-4xl xl:text-6xl font-black`}
-      >
-        {title}
-      </h1>
-      <>{children}</>
-    </div>
-  </div>
+  </article>
 )
 
 export default HeroComp
